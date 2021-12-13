ce4297c645076adb9f36cfa0b659bd15b60a6466
Thomas Trügler <thomas.truegler@imendo.at>
2021-12-13 08:17:59 +0100

    day 2 and day3

---
+++ Advent-of-Code-2021/day-2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace day_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt"));
+
+            ILookup<string, int> lookup =
+                      input.ToLookup(s => s.Split(' ')[0], s => Int32.Parse(s.Split(' ')[1]));
+
+            //part 1 
+            int y = lookup.Where(x => x.Key == "forward").SelectMany(x => x).Sum();
+            int x = lookup.Where(x => x.Key == "down").SelectMany(x => x).Sum()
+                - lookup.Where(x => x.Key == "up").SelectMany(x => x).Sum();
+
+            Console.WriteLine(x * y);
+
+            //part 2
+
+            long x2 = 0, y2 = 0, aim = 0;
+            int value = 0;
+            var data = input.Select(x => x.Split(' ')).ToArray();
+
+            foreach (var row in data)
+            {                
+                value = Int32.Parse(row[1]);
+                switch (row[0])
+                {
+                    case "up":
+                        aim -= value;
+                        break;
+                    case "down":
+                        aim += value;
+                        break;
+                    case "forward":
+                        x2 += value;
+                        y2 += value * aim;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine(x2 * y2);
+        }
+    }
+}
---
+++ Advent-of-Code-2021/day-3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace day_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Convert.ToString(c, toBase: 2)
+            string[] arr = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt"));
+
+            int[] counts = new int[arr[0].Length];
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                var numbers = arr[i].ToCharArray();
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    _ = numbers[j] == '1' ? counts[j]++ : counts[j]--;
+                }
+                
+            }
+
+            char[] gammarateArray = new char[arr[0].Length];
+            char[] epsilonrateArray = new char[arr[0].Length];
+
+            for (int i = 0; i < counts.Length; i++) 
+            {
+                if (counts[i] > 0)
+                {
+                    gammarateArray[i] = '1';
+                    epsilonrateArray[i] = '0';
+                }
+                else {
+                    gammarateArray[i] = '0';
+                    epsilonrateArray[i] = '1';
+                } 
+            }
+            
+            var gammarate = Convert.ToInt64(new string(gammarateArray), 2);
+            var epsilonrate = Convert.ToInt64(new string(epsilonrateArray),2);            
+
+            Console.WriteLine((int)gammarate * epsilonrate);
+
+        }
+    }
+}
